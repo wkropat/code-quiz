52b93226a6ae6c3d056d6835a2b3a8e6dbe41ee2
wkropat <wkropat@gmail.com>
2021-09-24 17:28:23 -0700

    added questions, answers, and options to js file

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// Add questions and answers
+
+const q1 = "Commonly used data types DO NOT include:";
+const a1 = "alerts";
+const options1 = ["strings", "booleans", "alerts", "numbers"];
+const q2 = "The condition in an if / else statement is enclosed within _____.";
+const a2 = "parentheses";
+const options2 = ["quotes", "curly brackets", "parentheses", "square brackets"];
+const q3 = "Arrays in JavaScript can be used to store _____.";
+const a3 = "all of the above";
+const options3 = ["numbers and strings", "other arrays", "booleans", "all of the above"];
