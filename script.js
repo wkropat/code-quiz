ad43dbf9dbf0ab40847b5b649771259519edd0ba
wkropat <wkropat@gmail.com>
2021-10-07 18:16:27 -0700

    got multiple choice questions to generate

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ var x = 0;
 
 // Data structure with questions, answers, and a key
 const questions = [{
-    "question": "Commonly used data types DO NOT include:",
-    "answer": "alerts",
-    "choices": ["strings", "booleans", "alerts", "numbers"]
+    question: "Commonly used data types DO NOT include:",
+    answer: "alerts",
+    choices: ["strings", "booleans", "alerts", "numbers"]
 }, {
-    "question": "The condition in an if / else statement is enclosed within _____",
-    "answer": "parentheses",
-    "choices": ["quotes", "curly brackets", "parentheses", "square brackets"]
+    question: "The condition in an if / else statement is enclosed within _____",
+    answer: "parentheses",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"]
 }, {
-    "question": "Arrays in JavaScript can be used to store _____.",
-    "answer": "all of the above",
-    "choices": ["numbers and strings", "other arrays", "booleans", "all of the above"]
+    question: "Arrays in JavaScript can be used to store _____.",
+    answer: "all of the above",
+    choices: ["numbers and strings", "other arrays", "booleans", "all of the above"]
 }];
 
 // Hide all content except the intro 
@@ -54,6 +54,141 @@ startButton.onclick = function () {
     //starts the timer when the start button is clicked
     countdown();
 }
+function nextQuestion(event) {
+    // Pulls up the next question, until end of questions, instead pull up initial form
+
+    if (x == 2) {
+        allDone()
+    } else {
+        x = x + 1;
+        askQuestion()
+    }
+}
+
+function incorrect(event) {
+    // Generate div below card saying answer is wrong
+    var resultEl = document.createElement("div");
+    resultEl.textContent = "Incorrect. Try again!";
+    multChoice.append(resultEl);
+    // Penalize the wrong answer by 10 seconds
+    timerCount = timerCount - 10;
+}
+
+
+// Fill out All Done! Card
+
+function allDone() {
+    multChoice.style.display = "none";
+    saveInit.style.display = "initial";
+
+
+    // for (let i = 0; i < question[x].choices[i]; i++) {
+    //     var choiceEl = document.createElement("button");
+    //     choiceEl.textContent = question[x].choices[i];
+    //     multChoice.appendChild(choice);
+
+    // }
+}
+// Fill out Question Card
+function askQuestion() {
+    // Ask multiple choice question
+    var questionEl = document.getElementById("questionText");
+    questionEl.textContent = questions[x].question;
+    // Generate answer buttons, with id=true or fale
+    for (let i = 0; i < 4; i++) {
+        var choiceEl = document.createElement("button");
+        choiceEl.textContent = questions[x].choices[i];
+        questionEl.append(choiceEl);
+        if (questions[x].choices[i] == questions[x].answer) {
+            choiceEl.setAttribute("id", "true");
+        } else {
+            choiceEl.setAttribute("id", "false");
+        }
+    }
+
+    var rightAnswer = document.getElementById("true")
+    var wrongAnswer = document.getElementById("false")
+
+    rightAnswer.onclick = function () {
+    // Pulls up the next question, until end of questions, instead pull up initial form
+
+    if (x == 2) {
+        allDone()
+    } else {
+        x = x + 1;
+        askQuestion()
+    }
+    }
+
+    wrongAnswer.onclick = function () {
+    // Generate div below card saying answer is wrong
+    var resultEl = document.createElement("div");
+    resultEl.textContent = "Incorrect. Try again!";
+    multChoice.append(resultEl);
+    // Penalize the wrong answer by 10 seconds
+    timerCount = timerCount - 10;
+
+    }
+
+
+
+    // rightAnswer.addEventListener("click", nextQuestion());
+    wrongAnswer.addEventListener("click", incorrect());
+
+}
+
+
+// Fill out High Score Card
+
+function highScore() {
+
+    document.getElementById("questionText").textContent = question[x].question;
+
+
+    for (let i = 0; i < question[0].choices[i]; i++) {
+        var choiceEl = document.createElement("div");
+        choiceEl.textContent = question[0].choices[i];
+        console.log(question[0].choices[i]);
+        multChoice.appendChild(choice);
+
+    }
+}
+
+// Start the quiz
+function startQuiz() {
+    startButton.disabled = true
+    timerCount = 60
+    countdown()
+    askQuestion()
+}
+
+// Event listeners below
+
+startButton.addEventListener("click", startQuiz);
+
+
+// If timer gets to zero go to high score page
+document.addEventListener("keypress", function (event) {
+    if (timerCount === 0) {
+        return
+    }
+});
+
+function setLose() {
+
+    timer.textContent = "Out of Time!";
+    multChoice.style.display = "none";
+    saveInit.style.display = "initial";
+    highScores.style.display = "none";
+    score.textContent = "You scored 0 points."
+
+}
+
+
+
+
+
+
 
 
 
@@ -122,115 +257,3 @@ startButton.onclick = function () {
 
 
 // }
-
-// Fill out Question Card
-function askQuestion() {
-    // Ask multiple choice question
-    var questionEl = document.getElementById("questionText");
-    console.log(questions[0].question);
-    questionEl.textContent = questions[x].question;
-
-
-    // Generate answer buttons, with id=true or fale
-    for (let i = 0; i < 4; i++) {
-
-
-
-        var choiceEl = document.createElement("button");
-        choiceEl.textContent = questions[x].choices[i];
-        questionEl.append(choiceEl);
-        if (questions[x].choices[i] == questions[x].answer) {
-            choiceEl.setAttribute("id", "true");
-        } else {
-            choiceEl.setAttribute("id", "false");
-        }
-    }
-
-    var rightAnswer = document.getElementById("true")
-    var wrongAnswer = document.getElementById("false")
-    rightAnswer.addEventListener("click", nextQuestion());
-    wrongAnswer.addEventListener("click", wrongAnswer());
-
-}
-
-function nextQuestion(event) {
-    // Pulls up the next question, until end of questions, instead pull up initial form
-
-    if (x == 3) {
-        allDone()
-    } else {
-        x = x + 1;
-        askQuestion()
-    }
-}
-
-function wrongAnswer(event) {
-    // Generate div below card saying answer is wrong
-    var resultEl = document.createElement("div");
-    resultEl.textContent = "Incorrect. Try again!";
-    multChoice.append(resultEl);
-    // Penalize the wrong answer by 10 seconds
-    timerCount = timerCount - 10;
-}
-
-
-
-// Fill out All Done! Card
-
-function allDone() {
-
-    document.getElementById("finishedText").textContent = question[x].question;
-
-
-    for (let i = 0; i < question[x].choices[i]; i++) {
-        var choiceEl = document.createElement("button");
-        choiceEl.textContent = question[x].choices[i];
-        multChoice.appendChild(choice);
-
-    }
-}
-// Fill out High Score Card
-
-function highScore() {
-
-    document.getElementById("questionText").textContent = question[x].question;
-
-
-    for (let i = 0; i < question[0].choices[i]; i++) {
-        var choiceEl = document.createElement("div");
-        choiceEl.textContent = question[0].choices[i];
-        console.log(question[0].choices[i]);
-        multChoice.appendChild(choice);
-
-    }
-}
-
-// Start the quiz
-function startQuiz() {
-    startButton.disabled = true
-    timerCount = 60
-    countdown()
-    askQuestion()
-}
-
-// Event listeners below
-
-startButton.addEventListener("click", startQuiz);
-
-
-// If timer gets to zero go to high score page
-document.addEventListener("keypress", function (event) {
-    if (timerCount === 0) {
-        return
-    }
-});
-
-function setLose() {
-
-    timer.textContent = "Out of Time!";
-    multChoice.style.display = "none";
-    saveInit.style.display = "initial";
-    highScores.style.display = "none";
-    score.textContent = "You scored 0 points."
-
-}
